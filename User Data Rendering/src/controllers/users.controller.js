f87f552b1f6ba850e6a7a7de69f2710875ba2ed7
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-01-09 21:49:16 +0530

    Add files via upload

---
+++ User Data Rendering/src/controllers/users.controller.js
@@ -0,0 +1,13 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+import { userModel } from "../models/users.model.js";
+
+export const userController = async (req, res) => {
+  // Write your code here
+  try {
+    const users = await userModel();
+    res.render("index", { users });
+  } catch (error) {
+    res.status(500).send("Internal Server Error");
+  }
+};
