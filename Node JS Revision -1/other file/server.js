cb02fe86dc36c5f1db977421f98fcd489e3eccf4
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2024-02-01 21:42:38 +0530

    Add files via upload

---
+++ Node JS Revision -1/other file/server.js
@@ -0,0 +1,16 @@
+export function  sum(x,y){
+    return  x + y;
+}
+
+// module.exports.sum=(x,y) =>{
+//     return x+y;
+// }
+
+export function div(x,y){
+    return x/y;
+}
+
+//common js module
+// module.exports = {
+//     add : sum
+// }
---
+++ Node JS Revision -1/other file/server2.js
@@ -0,0 +1,7 @@
+// const sumFile = require("./server.js");
+import * as sumFile from "./server.js";
+const result = sumFile.sum(10, 20);
+const result2 = sumFile.div(5, 20);
+console.log(result2);
+
+console.log(result);
